22d336a347a91521aa7457af3fa6c4c58cfdd82d
Hayk <haykavetisyan882@gmail.com>
2023-12-19 15:21:40 +0400

    start

---
+++ src/main/java/com/example/itevaluations/ItEvaluationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.itevaluations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItEvaluationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItEvaluationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itevaluation/ItEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItEvaluationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
