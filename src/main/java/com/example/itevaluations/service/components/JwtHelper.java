4dbad7b9ea67b5b3c479eebba993c72a0c7420d6
Hayk <haykavetisyan882@gmail.com>
2024-01-11 10:57:21 +0400

    fix deprication issues

--- src/main/java/com/example/itevaluations/service/components/JwtHelper.java
+++ src/main/java/com/example/itevaluations/service/components/JwtHelper.java
@@ -4,14 +4,13 @@ package com.example.itevaluations.service.components;
 import com.example.itevaluations.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -76,10 +75,13 @@ public class JwtHelper {
      * @return token
      */
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24))
+                .signWith(getSigningKey(), Jwts.SIG.HS256)
+                .compact();
     }
 
     /**
@@ -93,8 +95,6 @@ public class JwtHelper {
 
     /**
      * Get expiration time
-     *
-     * @param token
      * @return expiration time
      */
     private Date extractExpiration(String token) {
@@ -104,12 +104,13 @@ public class JwtHelper {
     /**
      * Extract claims from token
      *
-     * @param token
      * @return data
      */
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
-                .getBody();
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build().parseSignedClaims(token)
+                .getPayload();
     }
 
     /**
@@ -117,7 +118,7 @@ public class JwtHelper {
      *
      * @return key
      */
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
